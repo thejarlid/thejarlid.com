e6ea410d4ff50e7eb0e83b9de74e59fe7c6bc085
Dilraj Singh Devgun <thejarlid@meta.com>
2023-09-07 11:18:20 -0400

    fixed typo

--- pages/about.tsx
+++ pages/about.tsx
@@ -19,7 +19,7 @@ const AboutPage = () => {
           You can read about my prior experience at Instagram and Microsoft <a style={{fontWeight:"400"}} href="/dilraj-devgun-resume.pdf">here</a> . Sometimes I teach; 
           throughout college I was a TA for the intro progamming course and later the upper division OS course. Post-grad I volunteer occasionally to TA for Stanford&apos;s 
           Code In Place program. I would not be here without the patience of those who taught me and I feel as if its only right to pass it on. What started as a creative 
-          hobby of designing and developing iOS apps with my best friend grew into a technical interest and career pursuit. In general I love learning (and teaching and 
+          hobby of designing and developing iOS apps with my best friend grew into a technical interest and career pursuit. In general I love learning (and teaching) and 
           I use code as my medium to express my creativity.
           </p>
           <p style={{marginBottom:"1.5rem"}}>
